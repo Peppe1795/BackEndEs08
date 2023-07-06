e18684bb990720c98d834e69210054ba55af5f28
Peppe1795 <petrucci238@gmail.com>
2023-07-06 15:57:11 +0200

    inizio es 03

--- src/listaProdotti/App.java
+++ src/listaProdotti/App.java
@@ -14,23 +14,27 @@ public class App {
 				new Product(546456, "Angeli e demeoni", "Books", 25.50),
 				new Product(546456, "Social media", "Books", 350.25),
 				new Product(546456, "Il piccolo principe", "Books", 30.40),
-				new Product(651651, "Samsung S22", "Elettronica", 1800.50),
-				new Product(665165, "Pala", "Giardinaggio", 35.50), new Product(656151, "Pannolini", "Baby", 12.50),
-				new Product(651451, "Passeggino", "Baby", 500.12), new Product(515611, "Ballatoio", "Baby", 350.14)));
+				new Product(651651, "Giacca", "Boys", 450.10), new Product(665165, "Cravatta", "Boys", 125.40),
+				new Product(656151, "Pannolini", "Baby", 12.50), new Product(651451, "Passeggino", "Baby", 500.12),
+				new Product(515611, "Ballatoio", "Baby", 350.14)));
 
 		System.out.println("--------- Lista libri --------");
+		System.out.println();
 		Predicate<Product> isMoreThan100 = p -> p.getPrice() > 100;
 		Predicate<Product> isBooksCategory = p -> p.getCategory().equals("Books");
-		lista.stream().filter(isMoreThan100.and(isBooksCategory))
-				.forEach(s -> System.out.println("Id: " + s.getId() + " " + "nome libro: " + s.getName() + " "
-						+ "categoria: " + s.getCategory() + " " + "prezzo: " + s.getPrice()));
+		List<Product> listaProdotti = lista.stream().filter(isMoreThan100.and(isBooksCategory)).toList();
+		listaProdotti.forEach(s -> System.out.println("Id: " + s.getId() + " " + "nome libro: " + s.getName() + " "
+				+ "categoria: " + s.getCategory() + " " + "prezzo: " + s.getPrice()));
+		System.out.println();
 
 		System.out.println("--------- Lista ordini Baby -----------");
+		System.out.println();
 		Predicate<Product> isBabyCategory = p -> p.getCategory().equals("Baby");
 		List<Product> prodBaby = lista.stream().filter(isBabyCategory).toList();
 
 		Customer cliente1 = new Customer(6511651, "Antonio", 1);
 		Customer cliente2 = new Customer(6565111, "Luca", 2);
+
 		List<Order> ordini = new ArrayList<>(Arrays.asList(
 				new Order(6565156, "Pending", LocalDate.now().minusDays(2), LocalDate.now().plusDays(3), prodBaby,
 						cliente1),
@@ -39,6 +43,7 @@ public class App {
 		ordini.forEach(s -> System.out.println(
 				"Id: " + s.getId() + ", " + "status: " + s.getStatus() + ", " + "Data ordine " + s.getOrderDate() + ", "
 						+ "Data consegna: " + s.getDeliveryDate() + ", " + s.getProducts().toString()));
+		System.out.println();
 		System.out.println(cliente2.toString());
 		ordini.forEach(
 				s -> System.out.println("Id: " + s.getId() + ", " + "status: " + s.getStatus() + ", " + "Data ordine "
